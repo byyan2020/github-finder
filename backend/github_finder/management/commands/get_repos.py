f742fb2504bf231b4ed9dde60ae33c626df9d957
byan <yanbiyu2019@outlook.com>
2022-10-01 23:41:24 -0700

    Read username from command line; Store username and repo name in database

---
+++ backend/github_finder/management/commands/get_repos.py
@@ -0,0 +1,27 @@
+import imp
+from urllib import request
+from django.core.management.base import BaseCommand
+import requests
+from github_finder.models import UserName, Repository
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        username = input('Username:')
+
+        user_response = requests.get(
+            f"https://api.github.com/users/{username}").json()
+        # TODO: optimization on user input and validation
+        if "message" in user_response and user_response["message"] == "Not Found":
+            print("user not exsit")
+            return
+
+        repo_response = requests.get(
+            f"https://api.github.com/users/{username}/repos").json()
+
+        new_user = UserName(username=username)
+        new_user.save()
+        # TODO: optimization on save multiple entries
+        for repo in repo_response:
+            new_repo = Repository(repo_name=repo["name"], username=new_user)
+            new_repo.save()
